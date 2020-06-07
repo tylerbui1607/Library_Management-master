7f59c7fedaf09af9a5df522e3ffc9753d582c78f
tylerbui1607 <56749834+tylerbui1607@users.noreply.github.com>
2020-06-07 18:58:31 +0700

    DoAnCNPM

---
+++ Library_Management-master1/Library_Management/DAL/DBConnect.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+namespace Library_Management.DAL
+{
+    class DBConnect
+    {
+        protected
+            SqlConnection connection = new SqlConnection("Data Source=DESKTOP-PS7MU4U\\SQLEXPRESS;Initial Catalog=QLTV;Integrated Security=True");
+    }
+
+}
---
+++ Library_Management-master1/Library_Management/GUI/Login.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Library_Management.GUI
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public int Login_Result;
+        public Login()
+        {
+            InitializeComponent();
+
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            Login_Result = -1;
+            this.Close();
+        }
+        public void Logincheck()
+        {
+
+            if (Login_Result == 2)
+                MessageBox.Show("Dang nhap thanh cong");
+           
+          
+        }
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            DAL.DAL_LOGIN logincheck = new DAL.DAL_LOGIN();
+            if (logincheck.CheckLogin(username.Text, pass.Password))
+            {
+                Login_Result = 1;
+                this.Close();
+            }
+        
+             else
+                MessageBox.Show("Incorrect password or username!");
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+    }
+}
